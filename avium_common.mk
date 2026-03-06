b86b6d73a23fc531a184a0ecaa1c51f1424f7185
Artoria Pendragon <quietpulses@gmail.com>
2026-03-06 12:38:38 +0530

    Update avium_common.mk

--- avium_common.mk
+++ avium_common.mk
@@ -2,15 +2,15 @@ AVIUM_BUILDTYPE := Unoffical
 AVIUM_VERSION_APPEND_TIME_OF_DAY := false
 
 # GMS
-WITH_GMS = true
+WITH_GMS ?= true
 # TARGET_GMS_TYPE has three options: FULL, MINI and PICO
 # If WITH_GMS is true and TARGET_GMS_TYPE is not set, it will default to MINI
-TARGET_GMS_TYPE = FULL
+TARGET_GMS_TYPE ?= FULL
 
 # LatinIMEGooglePrebuilt
 # If WITH_GMS is true, TARGET_INCLUDE_GOOGLEIME and TARGET_GOOGLEIME_OVERRIDE_IME will be forced to true
-TARGET_INCLUDE_GOOGLEIME = true
-TARGET_GOOGLEIME_OVERRIDE_IME = true
+TARGET_INCLUDE_GOOGLEIME ?= true
+TARGET_GOOGLEIME_OVERRIDE_IME ?= true
 
 # It will display on the Setting-AboutPhone
 AVIUM_MAINTAINER := kagiyanagi
